b14ffc2d8d63438fd990d7130ae4ff6e2832d675
Wouter Kooyman van Guldener <wouter@wick-it.nl>
2016-05-24 13:01:16 +0200

    Removed validition of parents.

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Goal, type: :model do
 
   subject { goal }
 
-  it { should validate_presence_of(:parents) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:status) }
